4c2bdb3292f8428f9f8a1f873f70f3a85600b6fd
Daniel ciroy <142291477+DanielCiroy@users.noreply.github.com>
2024-04-30 12:14:23 +0200

    cour de js

---
+++ TEST/app.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+let age = prompt('entrez votre age');
+
+if (age < 10) {
+    alert('salut gamin');
+} else if (age >= 10 && age <= 18) {
+    alert('salut grand');
+} else {
+    alert('mes respect');
+}
