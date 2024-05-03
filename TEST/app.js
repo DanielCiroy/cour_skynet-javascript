e942b1b28a7ae6b522c5948ca2d23360f8d01877
Daniel ciroy <142291477+DanielCiroy@users.noreply.github.com>
2024-05-03 11:24:19 +0200

    functions & arrays

--- TEST/app.js
+++ TEST/app.js
@@ -1,14 +1,23 @@
+/*
+let nombre1 = parseInt(prompt('donner le premier nombre'));
+let nombre2 = parseInt(prompt('donner le deuxieume nombre'));
+function addition(a, b) {
+    if (parseInt(a) && parseInt(b)) {
+        return alert(a + b);
+    }
+    else {
+        alert('veuillez saisir des nombres entier')
+    }
 
-
-
-
-
-let age = prompt('entrez votre age');
-
-if (age < 10) {
-    alert('salut gamin');
-} else if (age >= 10 && age <= 18) {
-    alert('salut grand');
-} else {
-    alert('mes respect');
 }
+
+
+addition(nombre1, nombre2)
+*/
+
+tab = ['dani', 'john', 'ciroy'];
+
+for (let i = 0; i <= tab.length; i++) {
+    console.log(tab[i]);
+}
+
